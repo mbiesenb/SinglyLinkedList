7a86478f2a27722c59e19f52af06922b92a69693
marvin.biesenbach@gmx.de <marvin.biesenbach@gmx.de>
2017-06-30 16:47:08 +0200

    Added commentar

--- src/LL/App.java
+++ src/LL/App.java
@@ -11,6 +11,6 @@ package LL;
  */
 public class App {
     public static void main(String[] args){
-        System.out.println("LL.App.main()");
+        //der kommentar ist ein Test für Git Hub
     }
 }
